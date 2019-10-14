f31f015c1f23320a21984b39f782a192add924b3
Said Ali <said-ali1997@hotmail.com>
2019-10-14 12:22:22 +0200

    added form-group and /-control

--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,8 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
+  //Controll over multiple values
+  registrationForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    birthday: new FormControl(''),
+    adress: new FormControl(''),
+    number: new FormControl(''),
+    serviceType: new FormControl('')
+  });
+
   constructor() { }
 
   ngOnInit() {}
 
+  onSubmit(){
+    // TODO: Use EventEmitter with form value
+  console.warn(this.registrationForm.value);
+  }
+
+  //not used yet
+  get serviceType() {
+    return this.registrationForm.get('serviceType');
+  }
 }
