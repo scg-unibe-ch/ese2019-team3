c3f62883efa244c64bfb7a07c9c202fad1b82b04
Said Ali <said-ali1997@hotmail.com>
2019-10-14 16:34:23 +0200

    form input is saved locally and validations of required fields

--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-registration',
@@ -8,27 +9,48 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class RegistrationComponent implements OnInit {
 
+  
   //Controll over multiple values
   registrationForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    birthday: new FormControl(''),
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    birthday: new FormControl('', Validators.required),
     adress: new FormControl(''),
     number: new FormControl(''),
-    serviceType: new FormControl('')
+    //TODO Validate 
+    serviceType: new FormControl('') 
+
   });
 
-  constructor() { }
 
-  ngOnInit() {}
+  //in progress
+  constructor(private http: HttpClient) { 
 
+    const url = 'http://localhost:4200/registration';
+    this.http.post(url, this.registrationForm.value).subscribe(
+      () => {},
+      (e) => console.error(e)
+
+    )
+  }
+
+  // testing 
+  ngOnInit() {
+    this.registrationForm.valueChanges.subscribe(
+      (value) => console.log(value),
+    )
+  }
+
+  // testing 
   onSubmit(){
     // TODO: Use EventEmitter with form value
-  console.warn(this.registrationForm.value);
+    console.warn(this.registrationForm.value);
+
   }
 
   //not used yet
   get serviceType() {
     return this.registrationForm.get('serviceType');
+    
   }
 }
