3edc7697b1d62f08d988a9c0d18ff504fbc2ad82
Said Ali <said-ali1997@hotmail.com>
2019-10-28 23:43:09 +0100

    slight changes in backend requests and added form completion

--- frontend/src/app/authentication.service.ts
+++ frontend/src/app/authentication.service.ts
@@ -8,11 +8,23 @@ export class AuthenticationService {
 
   private registerUrl = "http://localhost:3000/user/register"
 
+  private passwordforgottenUrl = "http://localhost:3000/user/forgotPassword"
   constructor(private http: HttpClient) {
   }
 
   //accepts userObject and returns response of backend, backend responses either with error or registered user
-  registerUser(user){
+  registerUser(user: Object){
     return this.http.post<any>(this.registerUrl, user)
+  }
+
+
+   /** 
+   * Initiate the passwordforgotten
+   * sends put request to the backend 
+   * to replace old password
+   * @param email email of the user 
+   */ 
+  passwordForgotten(email: Object){
+    return this.http.put<any>(this.passwordforgottenUrl, email)
   }
 }
