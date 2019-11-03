a9a2d4362375e9b59ddcfc076c6e41c5496123c4
Kerrie Lesli Stauffer <kerrie.stauffer@students.unibe.ch>
2019-11-03 16:38:55 +0100

    added user validation admin page

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -34,17 +34,14 @@ export class AdminComponent implements OnInit {
       },
       ]
   }
-//TODO: secure path to admin page and let only admins see it
-  // TODO: gets Requests from backend WIP
+
   getRegistrationRequests(){
     return this.http.get<User[]>("http://localhost:3000/user/verified");
   }
-  /*getRegistrationRequests(){
-  return this.http.get<User[]>(`${config.apiUrl}/users`);
-    this.HttpClient.get(this.adminUrl).subscribe((instances: any) => {
-      this.Users = instances.map((instance) => new User(instance.username, instance.password, instance.firstName, instance.lastName, instance.phone, instance.userGroups, instance.token));
-    });
-  }*/
+
+  validateUser(user: User){
+    return "http://localhost:3000/user/validate/"+user.id;
+  }
 
   ngOnInit() {}
 
