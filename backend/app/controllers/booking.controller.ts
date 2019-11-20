edc3af3e3a6c1c2c2c37e9509448ad598c1b3c78
jufrank <frank_julian@hotmail.com>
2019-11-20 23:27:58 +0100

    added more methods on booking controller

--- backend/app/controllers/booking.controller.ts
+++ backend/app/controllers/booking.controller.ts
@@ -2,6 +2,7 @@ import {Request, Response, Router} from 'express';
 import {Booking} from '../models/booking.model';
 import {User} from '../models/user.model';
 import {Service} from '../models/service.model';
+import {Op} from 'sequelize';
 
 const router: Router = Router();
 
@@ -10,6 +11,42 @@ router.get('/', async (req: Request, res: Response) => {
     const booking = await Booking.findAll();
 
      res.send(booking);
+});
+
+/**
+ * Get all bookings a specific client made (including accepted and declined one, past and future ones
+ *
+ */
+router.get('/client/:id', async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const booking = await Booking.findAll({where: {clientId: id} }); //https://sequelize-guides.netlify.com/search-operators/
+    res.statusCode = 200;
+    res.send(booking);
+});
+
+
+/**
+ * Get all bookings a specific provider made (including accepted and declined one, past and future ones
+ *
+ */
+router.get('/client/:id', async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const booking = await Booking.findAll({where: {providerId: id} }); //https://sequelize-guides.netlify.com/search-operators/
+    res.statusCode = 200;
+    res.send(booking);
+});
+
+
+/**
+ * Get all bookings a specific user made, that have been accepted, and that are in the past (the service has already been done)
+ *
+ */
+router.get('/rate/client/:id', async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    //https://sequelize-guides.netlify.com/search-operators/
+    const booking = await Booking.findAll({where: {date: { [Op.lte]: Date.now()}, clientId: id, bookingStatus: 'accepted'} });
+    res.statusCode = 200;
+    res.send(booking);
 });
 
 router.post('/register', async (req: Request, res: Response) => {
@@ -64,6 +101,22 @@ async function updateRating(providerId: number) {
     console.log('no bookings found');
     }
 }
+
+router.put('/accept/:id', async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const booking = await Booking.findOne({where: {id: id}});
+    if (booking!=null){
+    booking.bookingStatus = 'accepted';
+    await booking.save().then ( async() => {
+    });
+    res.send('booking accepted');
+    res.status(200);
+    } else {
+        res.statusCode = 400;
+        res.send('Booking not found');
+    }
+
+});
 
 export const BookingController: Router = router;
 
