99d4ab69535459c035a28c5de542cf8ae0abdad8
Said Ali <said-ali1997@hotmail.com>
2019-11-27 20:38:26 +0100

    Add ProfileButton in header and hover Effect on title

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,33 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthenticationService} from '../authentication.service';
-import { AdminGuard} from '../admin.guard';
-import { Router} from '@angular/router';
-import {UserGroupGuard} from '../user-group.guard';
+import { Component, OnInit } from "@angular/core";
+import { AuthenticationService } from "../authentication.service";
+import { AdminGuard } from "../admin.guard";
+import { Router } from "@angular/router";
+import { UserGroupGuard } from "../user-group.guard";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
   loggedIn = this.auth.loggedIn();
   isAdmin = this.adminGuard.isAdmin();
   isProvider = this.providerGuard.isProvider();
   innerWidth: any;
-  public firstname :string;
+  public firstname: string;
+  public lastname: string;
+  public profileId: string;
 
-  constructor(private auth: AuthenticationService, private adminGuard: AdminGuard, private router: Router, private providerGuard: UserGroupGuard) {
-  }
+  constructor(
+    private auth: AuthenticationService,
+    private adminGuard: AdminGuard,
+    private router: Router,
+    private providerGuard: UserGroupGuard
+  ) {}
 
   ngOnInit() {
     this.innerWidth = window.innerWidth;
     this.getUsername();
+    this.getProfileId();
   }
 
-  getUsername(){
-      this.firstname = this.auth.getCurrentUser().firstname;
+  getUsername() {
+    this.firstname = this.auth.getCurrentUser().firstname;
+    this.lastname = this.auth.getCurrentUser().lastname;
   }
 
+  getProfileId() {
+    this.profileId =
+      this.firstname.toUpperCase().charAt(0) +
+      this.lastname.toUpperCase().charAt(0);
+  }
   logIn() {
     this.loggedIn = this.auth.loggedIn();
     this.isAdmin = this.adminGuard.isAdmin();
@@ -36,11 +49,11 @@ export class HomeComponent implements OnInit {
 
   logOut() {
     // Test
-    alert('Sie wurden erfolgreich abgemeldet');
+    alert("Sie wurden erfolgreich abgemeldet");
     this.auth.logOutUser();
     this.isAdmin = this.adminGuard.isAdmin();
     this.loggedIn = this.auth.loggedIn();
     this.isProvider = this.providerGuard.isProvider();
-    this.router.navigate(['']);
+    this.router.navigate([""]);
   }
 }
